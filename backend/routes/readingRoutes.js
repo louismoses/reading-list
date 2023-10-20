c98a6efeb3d60125e950e4ce5cbc0cdb6c5a8453
Louis Moses <louismoses.rait@gmail.com>
2023-10-20 18:18:08 +0800

    update: get book by ID

--- backend/routes/readingRoutes.js
+++ backend/routes/readingRoutes.js
@@ -42,4 +42,16 @@ router.get("/", async (req, res) => {
   }
 });
 
+//get book by Id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
 export default router;
