f2f2c8a39e8b19446c76a385733628100c647ff2
Louis Moses <louismoses.rait@gmail.com>
2023-10-21 10:28:01 +0800

    update: edit book info

--- backend/routes/readingRoutes.js
+++ backend/routes/readingRoutes.js
@@ -42,12 +42,33 @@ router.get("/", async (req, res) => {
   }
 });
 
-//get book by Id
+// route to get book by ID
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const book = await Book.findById(id);
     return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// route to update a book
+router.put("/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({ message: "Send all required fields" });
+    }
+
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not Found" });
+    }
+    return res.status(200).send({ message: "Book updated successfully" });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
