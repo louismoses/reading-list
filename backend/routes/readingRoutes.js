381b99b50c66ab797a81f2b58c099d0f16f37d71
Louis Moses <louismoses.rait@gmail.com>
2023-10-21 10:32:58 +0800

    update: remove book from list

--- backend/routes/readingRoutes.js
+++ backend/routes/readingRoutes.js
@@ -75,4 +75,21 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// remove to list
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
 export default router;
