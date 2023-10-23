0dcc34d7692a16746849e163e7e686852144a6bf
Louis Moses <louismoses.rait@gmail.com>
2023-10-23 17:51:59 +0800

    add navbar

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,25 +3,7 @@ import Spinner from "./Spinner";
 import axios from "axios";
 
 const Hero = () => {
-  const [quotes, setQuotes] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get("https://type.fit/api/quotes")
-      .then((response) => {
-        setQuotes(response.data);
-        console.log(response.data[0].text);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err.message);
-        setLoading(false);
-      });
-  }, []);
-
-  return <section></section>;
+  return <section>HEro</section>;
 };
 
 export default Hero;
