2f9c9038d718602d9314519d93f68b3fc6e17141
Louis Moses <louismoses.rait@gmail.com>
2023-10-24 20:01:02 +0800

    display books as card

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -1,7 +1,11 @@
 const BookSingleCard = ({ book }) => {
   return (
     <div key={book._id} className=" max-w-[230px] border rounded-lg">
-      <img src="https://placehold.co/400x400" alt={book.title} className="" />
+      <img
+        src="https://placehold.co/228x228"
+        alt={book.title}
+        className="rounded-t-lg"
+      />
       <div className="py-2 px-3">
         <h2 className=" text-2xl font-bold uppercase">{book.title}</h2>
         <h3 className="text-xl ">Author : {book.author}</h3>
