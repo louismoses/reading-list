c7a4ed61544db6a50f82c703c83506ef37399afb
Louis Moses <louismoses.rait@gmail.com>
2023-10-24 17:35:47 +0800

    add hero section and stylings

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,14 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Heading from "../components/home/Heading";
-
-import { useState } from "react";
 
 import BooksCard from "../components/home/BooksCard";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Navbar />
       <Hero />
-      <Heading />
-    </div>
+    </main>
   );
 };
 
