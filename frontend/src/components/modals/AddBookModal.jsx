70501378dbedb7d44e88482cfe63f5b33802277a
Louis Moses <louismoses.rait@gmail.com>
2023-10-27 11:37:16 +0800

    update: add style on AddBookModal

--- frontend/src/components/modals/AddBookModal.jsx
+++ frontend/src/components/modals/AddBookModal.jsx
@@ -1,8 +1,11 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { AiOutlineCloseCircle } from "react-icons/ai";
-import axios from "axios";
-import Spinner from "../Spinner";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSnackbar } from "notistack";
+import { FcNews } from "react-icons/fc";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import Spinner from "../Spinner";
+import axios from "axios";
+import { useFormContext } from "react-hook-form";
 
 const AddBookModal = ({ onClose }) => {
   const [newBook, setNewBook] = useState({
@@ -13,17 +16,27 @@ const AddBookModal = ({ onClose }) => {
     note: "",
   });
 
+  const { enqueueSnackbar } = useSnackbar();
+
   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: (newBook) => {
       return axios
         .post("http://localhost:3000/books", newBook)
-        .then(() => queryClient.invalidateQueries({ queryKey: ["books"] }));
+        .then(() => {
+          queryClient.invalidateQueries({ queryKey: ["books"] });
+          enqueueSnackbar("Added Successfully", { variant: "success" });
+          onClose();
+        })
+        .catch((error) => {
+          enqueueSnackbar("Check on required fields", {
+            variant: "error",
+          });
+        });
     },
   });
   const handleSave = () => {
     mutation.mutate(newBook);
-    onClose();
   };
 
   function handleChange(e) {
@@ -43,63 +56,98 @@ const AddBookModal = ({ onClose }) => {
     >
       <div
         onClick={(event) => event.stopPropagation()}
-        className="w-[600px] max-w-full min-h-[400px]  bg-white rounded-xl p-4 flex-col relative"
+        className="max-w-[600px] max-w-full min-h-[400px]  bg-white rounded-xl p-4 flex-col relative"
       >
         <AiOutlineCloseCircle
           className="text-red-800 absolute right-4 top-4 cursor-pointer text-2xl"
           onClick={onClose}
         />
-        <h3 className="text-4xl uppercase">New Book</h3>
-        <hr />
+        <div className="flex place-items-center gap-2">
+          <FcNews className="text-3xl" />
+          <h3 className="text-3xl uppercase text-slate-800">New Book</h3>
+        </div>
         <div>
           {mutation.isLoadding ? (
             <Spinner />
           ) : (
             <>
-              {mutation.isError ? <div>{mutation.error.message}</div> : null}
-              {mutation.isSuccess ? <div>New Book added</div> : null}
               <div>
+                <label htmlFor="title" className="text-sm text-gray-400 italic">
+                  Title:{" "}
+                  <span className="text-xs text-red-300">
+                    {!newBook.title && "(required)"}
+                  </span>
+                </label>
                 <input
                   value={newBook.title}
                   placeholder="Title"
                   name="title"
                   onChange={handleChange}
-                  className="border w-full text-black"
+                  className="border border-green-700 p-2 my-1 w-full rounded-lg "
                 />
+                <label
+                  htmlFor="author"
+                  className="text-sm text-gray-400 italic"
+                >
+                  Author:{" "}
+                  <span className="text-xs text-red-300">
+                    {!newBook.title && "(required)"}
+                  </span>
+                </label>
                 <input
                   value={newBook.author}
                   placeholder="Author"
                   name="author"
                   onChange={handleChange}
-                  className="border w-full text-black"
+                  className="border border-green-700 p-2 my-1 w-full rounded-lg "
                 />
+                <label htmlFor="title" className="text-sm text-gray-400 italic">
+                  Publish Year:{" "}
+                  <span className="text-xs text-red-300">
+                    {!newBook.title && "(required)"}
+                  </span>
+                </label>
                 <input
                   value={newBook.publishYear}
                   placeholder="Publish Year"
                   name="publishYear"
                   onChange={handleChange}
                   type="number"
-                  className="border w-full text-black"
+                  className="border border-green-700 p-2 my-1 w-full rounded-lg"
                 />
+                <label
+                  htmlFor="status"
+                  className="text-sm text-gray-400 italic"
+                >
+                  Status:
+                </label>
                 <select
                   name="status"
                   onChange={handleChange}
                   value={newBook.status}
-                  className="border w-full text-black"
+                  className="border border-green-700 p-2 my-1 w-full rounded-lg"
                 >
                   <option value="To Read">To Read</option>
                   <option value="In Progress">In Progress</option>
                   <option value="Done">Done</option>
                 </select>
+                <label htmlFor="notes" className="text-sm text-gray-400 italic">
+                  Personal note:
+                </label>
                 <textarea
                   name="note"
                   cols="100%"
                   rows="10"
                   defaultValue={newBook.note}
                   onChange={handleChange}
-                  className="border w-full text-black"
+                  className="border border-green-700 p-2 my-1 w-full rounded-lg"
                 ></textarea>
-                <button onClick={handleSave}>Save</button>
+                <button
+                  onClick={handleSave}
+                  className="p-2 bg-green-500 w-full rounded-lg text-white uppercase"
+                >
+                  Save
+                </button>
               </div>
             </>
           )}
