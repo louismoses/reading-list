4f75a40a83d1b068391b2e5daa9e9e48ef09ab5c
Louis Moses <louismoses.rait@gmail.com>
2023-10-27 15:33:42 +0800

    update: style BookDetailModal

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -13,7 +13,11 @@ const BookSingleCard = ({ book }) => {
 
   return (
     <section>
-      <div key={book._id} className=" max-w-[230px] border rounded-lg">
+      <div
+        key={book._id}
+        className=" max-w-[230px] border rounded-lg"
+        onClick={() => setShowBookDetail(true)}
+      >
         <img
           src="https://img.freepik.com/free-photo/old-books-with-white-background_23-2148898329.jpg?w=1380&t=st=1698385817~exp=1698386417~hmac=b29020de5ff122cd96f0e49186c93f19affd5faa277462dbb0a68c7831dd48ea"
           alt={book.title}
@@ -22,8 +26,12 @@ const BookSingleCard = ({ book }) => {
           height="228px"
         />
         <div className="py-2 px-3">
-          <h2 className=" text-2xl font-bold uppercase">{book.title}</h2>
-          <h3 className="text-xl ">Author : {book.author}</h3>
+          <h2 className=" text-2xl font-bold uppercase overflow-hidden  max-h-9">
+            {book.title}
+          </h2>
+          <h3 className="text-xl overflow-hidden  max-h-8">
+            Author : {book.author}
+          </h3>
           <p>Year Published: {book.publishYear}</p>
           <p>Status: {book.status}</p>
         </div>
--- frontend/src/components/modals/BookDetailModal.jsx
+++ frontend/src/components/modals/BookDetailModal.jsx
@@ -8,7 +8,7 @@ const BookDetailModal = ({ book, onClose }) => {
     >
       <div
         onClick={(event) => event.stopPropagation()}
-        className="w-[600px] max-w-full min-h-[400px]  bg-white rounded-xl p-4 flex-col relative"
+        className="w-[600px] max-w-full border-sky-300 border-4    bg-white rounded-xl p-4 flex-col relative"
       >
         <AiOutlineCloseCircle
           className="text-red-800 absolute right-4 top-4 cursor-pointer text-2xl"
@@ -16,27 +16,28 @@ const BookDetailModal = ({ book, onClose }) => {
         />
         <div className="flex place-items-center gap-2">
           {/* <FcNews className="text-3xl" /> */}
-          <h2 className="text-3xl uppercase text-slate-800 p-2 flex-wrap">
+          <h2 className="text-3xl uppercase text-slate-800 p-2 flex-wrap overflow-auto max-h-16">
             {book.title}
           </h2>
         </div>
-        <div className="p-2 my-1 w-full  border-b flex place-items-center">
-          <span className="mr-1">Author : </span>
-          <h3> {book.author}</h3>
-        </div>
-        <div className="p-2 my-1 w-full  border-b flex place-items-center">
+
+        <h3 className="overflow-auto max-h-16 p-2 my-1 w-full border rounded-lg border-sky-300">
+          <span className="mr-1">Author : </span> {book.author}
+        </h3>
+
+        <div className="p-2 my-1 w-full  border border-sky-300 rounded-lg flex place-items-center">
           <span className="mr-1">Publish Year : </span>
           <h3> {book.publishYear}</h3>
         </div>
-        <div className="p-2 my-1 w-full   flex place-items-center">
+        <div className="p-2 my-1 w-full rounded-lg border  border-sky-300 flex place-items-center">
           <span className="mr-1">Status : </span>
           <h3> {book.status}</h3>
         </div>
 
-        <p className="max-h-[130px] overflow-auto border p-1.5">
+        <p className="max-h-[130px] min-h-10 overflow-auto border border-sky-300 rounded-lg p-1.5">
           <span className="italic">Note :</span> {book.note}
         </p>
-        <div className=" absolute bottom-2 text-sm italic">
+        <div className="  text-sm italic pt-2">
           <p>
             Last Updated : <span>{book.updatedAt}</span>
           </p>
